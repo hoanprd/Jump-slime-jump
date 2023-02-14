234f59b819cea89b5e5aa6d46e92d424fa1b422a
hoanprd <99530243+hoanprd@users.noreply.github.com>
2023-02-14 21:26:49 +0700

    CF 14.02.2023 P1

---
+++ Jump slime jump/Assets/Script/Slime.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : MonoBehaviour
+{
+    public float move_speed;
+    float x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        float move_step = move_speed * x * Time.deltaTime;
+        transform.position = transform.position + new Vector3(move_step, 0 - Time.deltaTime, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Crossbar"))
+        {
+            transform.position = transform.position + new Vector3(0, 1f, 0);
+        }
+    }
+}
