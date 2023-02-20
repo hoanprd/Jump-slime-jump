1d06aeced96ee8d6cac476227b6c9561028d40dc
hoanprd <99530243+hoanprd@users.noreply.github.com>
2023-02-20 20:41:48 +0700

    CF 20.02.2023 P1

--- Jump slime jump/Assets/Script/Slime.cs
+++ Jump slime jump/Assets/Script/Slime.cs
@@ -6,6 +6,7 @@ public class Slime : MonoBehaviour
 {
     public float move_speed;
     float x;
+    bool moveL, moveR;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,20 @@ public class Slime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        x = Input.GetAxisRaw("Horizontal");
+        if (moveL == true)
+        {
+            x = -1;
+        }
+        else if (moveR == true)
+        {
+            x = 1;
+        }
+        else
+        {
+            x = 0;
+        }
+
+        //x = Input.GetAxisRaw("Horizontal");
         float move_step = move_speed * x * Time.deltaTime;
         transform.position = transform.position + new Vector3(move_step, 0 - Time.deltaTime, 0);
     }
@@ -27,11 +41,6 @@ public class Slime : MonoBehaviour
         {
             transform.position = transform.position + new Vector3(0, Time.deltaTime * 70f, 0);
         }
-        /*if (collision.gameObject.CompareTag("Rock"))
-        {
-            GameController.dead = true;
-            Destroy(gameObject);
-        }*/
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -51,5 +60,15 @@ public class Slime : MonoBehaviour
             GameController.dead = true;
             Destroy(gameObject);
         }
+    }
+
+    public void MovementL(bool _move)
+    {
+        moveL = _move;
+    }
+
+    public void MovementR(bool _move)
+    {
+        moveR = _move;
     }
 }
